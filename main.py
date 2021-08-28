91e31667477fd2fd32b0a0bed75d3e97824d79ec
Adrian Katulski <katulskiadrian@gmail.com>
2021-08-28 17:47:32 +0200

    dodany symulator urządzenia

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi_mqtt import FastMQTT, MQTTConfig
 
 MQTTnewMsg = 0
-zmienna = 0
+MQTTdata = 0
 
 app = FastAPI()
 
@@ -27,22 +27,19 @@ mqtt.init_app(app)
 
 templates = Jinja2Templates(directory="templates")
 
-with open('measurements.json', 'r') as file:
-    measurements = iter(json.loads(file.read()))
-
 @mqtt.on_connect()
 def connect(client, flags, rc, properties):
-    mqtt.client.subscribe("/mqtt") #subscribing mqtt topic
+    mqtt.client.subscribe("data") #subscribing mqtt topic
     print("Connected: ", client, flags, rc, properties)
 
 @mqtt.on_message()
 async def message(client, topic, payload, qos, properties):
     print("Received message: ",topic, payload.decode(), qos, properties)
-    global zmienna
+    global MQTTdata
     global MQTTnewMsg
     MQTTnewMsg = 1
-    zmienna = int(payload.decode())
-    print(zmienna)
+    MQTTdata = json.loads(payload.decode())
+    print(MQTTdata['deviceNr'])
 
 @mqtt.on_disconnect()
 def disconnect(client, packet, exc=None):
@@ -58,16 +55,16 @@ def read_root(request: Request):
 
 @app.get("/zmienna")
 async def root():
-    global zmienna
-    return {"zmienna": zmienna}
+    global MQTTdata
+    return {MQTTdata}
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        global zmienna
+        global MQTTdata
         global MQTTnewMsg
         await asyncio.sleep(0.5)
         if(MQTTnewMsg == 1):
             MQTTnewMsg = 0
-            await websocket.send_json({"value" : zmienna})
+            await websocket.send_json({"value" : MQTTdata['rotationSpeed']})
