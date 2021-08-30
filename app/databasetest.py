63695604647ef4f8b5fc09170812106c74691818
Adrian Katulski <katulskiadrian@gmail.com>
2021-08-30 21:33:02 +0200

    Wrzucona machenika bazy danych do pliku databasetest.py oraz dodane zapisywanie danych do bazy danych z callbacka mqtt

---
+++ app/databasetest.py
@@ -0,0 +1,81 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime, TIMESTAMP
+from sqlalchemy.dialects.mysql import DATETIME
+from sqlalchemy.sql import func
+import datetime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+engine = create_engine('sqlite:///./dat.db', echo=True) #generujemy silnik bazodanowy to połączenia
+#baza danych ma problem z samodzielnym generowaniem - żałosne
+
+class dataFromDevices(Base): #generujemy następny model tabeli
+  __tablename__ = 'dataFromDevices'
+  
+  id = Column(Integer, primary_key=True)
+  #timestamp = Column(DATETIME(fsp=6), default=func.now(), onupdate=func.now())
+  deviceNr = Column(Integer)
+  fuel = Column(Integer)
+  rotationSpeed = Column(Integer)
+  speed = Column(Integer)
+  voltage = Column(Float)
+  
+  #addresses = relationship("Address", order_by="Address.id", backref="user")
+  def __init__(self, deviceNr, fuel, rotationSpeed, speed, voltage):
+      self.deviceNr = deviceNr
+      self.fuel = fuel
+      self.rotationSpeed = rotationSpeed
+      self.speed = speed
+      self.voltage = voltage
+
+class cars(Base):
+  __tablename__ = 'cars'
+  
+  id = Column(Integer, primary_key=True)
+  carName = Column(String)
+  fuel = Column(Integer)
+  rotationSpeed = Column(Integer)
+  speed = Column(Integer)
+  voltage = Column(Float)
+  
+  #addresses = relationship("Address", order_by="Address.id", backref="user")
+  def __init__(self, deviceNr, fuel, rotationSpeed, speed, voltage):
+      self.deviceNr = deviceNr
+      self.fuel = fuel
+      self.rotationSpeed = rotationSpeed
+      self.speed = speed
+      self.voltage = voltage
+
+
+Base.metadata.create_all(engine) #generujemy tabele w bazie danych
+
+'''
+class cars(Base): #generujemy model tabeli
+  __tablename__ = 'cars'
+  id = Column(Integer, primary_key=True)
+  email_address = Column(String, nullable=False)
+  user_id = Column(Integer, ForeignKey('users.id'))
+  user = relationship("User", backref=backref('addresses', order_by=id))
+'''  
+  
+'''
+  Session = sessionmaker(bind=engine)
+  session = Session()
+  ed_user = User('ed', 'Ed Jones', 'edspassword')
+  ed_user.addresses = [ Address(email_address='jack@google.com'), Address(email_address='j25@yahoo.com')]
+  session.add_all([ed_user,])
+  session.commit()
+  for instance in session.query(User).order_by(User.id):
+    print('%s - %s' % (instance.name, str(instance.addresses)))
+'''
+
+
+
+
+
+
+
--- app/main.py
+++ app/main.py
@@ -3,18 +3,14 @@
 import json
 import asyncio
 import uvicorn
+import app.databasetest as databasetest
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi import WebSocket
 from fastapi.templating import Jinja2Templates
 ##############sql#################
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import sessionmaker
 #from app.database import SessionLocal, engine
 
 ############mqtt###############
@@ -34,6 +30,7 @@ MQTTdata = 0
 #saveData()
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 mqtt_config = MQTTConfig(host = "localhost",
     port= 1883,
@@ -45,9 +42,16 @@ mqtt = FastMQTT(
 )
 
 mqtt.init_app(app)
+Session = sessionmaker(bind=databasetest.engine)
+session = Session()
 
 
-templates = Jinja2Templates(directory="templates")
+def saveToDb(deviceNr, fuel, rotationSpeed, speed, voltage):
+    global session
+    tr = databasetest.dataFromDevices(deviceNr, fuel, rotationSpeed, speed, voltage)
+    session.add(tr)
+    session.commit()
+
 
 
 
@@ -60,10 +64,13 @@ def connect(client, flags, rc, properties):
 @mqtt.on_message()
 async def message(client, topic, payload, qos, properties):
     print("Received message: ",topic, payload.decode(), qos, properties)
+    #saveToDb()
     global MQTTdata
     global MQTTnewMsg
     MQTTnewMsg = 1
     MQTTdata = json.loads(payload.decode())
+    print(type(MQTTdata))
+    saveToDb(MQTTdata['deviceNr'], MQTTdata['fuel'], MQTTdata['rotationSpeed'], MQTTdata['speed'],MQTTdata['voltage'],)
 
 @mqtt.on_disconnect()
 def disconnect(client, packet, exc=None):
@@ -96,52 +103,3 @@ async def websocket_endpoint(websocket: WebSocket):
 
 ####################################sql
 
-Base = declarative_base()
-
-class User(Base):
-  __tablename__ = 'users'
-  
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  fullname = Column(String)
-  password = Column(String)
-  
-  #addresses = relationship("Address", order_by="Address.id", backref="user")
-  def __init__(self, name, fullname, password):
-      self.name = name
-      self.fullname = fullname
-      self.password = password
-
-  def __repr__(self):
-     return "<User('%s','%s', '%s')>" % (self.name, self.fullname, self.password)
-
-class Address(Base):
-  __tablename__ = 'addresses'
-  id = Column(Integer, primary_key=True)
-  email_address = Column(String, nullable=False)
-  user_id = Column(Integer, ForeignKey('users.id'))
-  user = relationship("User", backref=backref('addresses', order_by=id))
-
-  def __init__(self, email_address):
-      self.email_address = email_address
-
-  def __repr__(self):
-      return "<Address('%s')>" % self.email_address
-
-engine = create_engine('sqlite:///:memory:', echo=True)
-
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-ed_user = User('ed', 'Ed Jones', 'edspassword')
-ed_user.addresses = [
- Address(email_address='jack@google.com'),
- Address(email_address='j25@yahoo.com')]
-session.add_all([ed_user,])
-session.commit()
-
-
-for instance in session.query(User).order_by(User.id):
-  print ('%s - %s' % (instance.name, str(instance.addresses)))
